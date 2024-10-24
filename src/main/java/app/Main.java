0fc62dc818b78bdc469ac4f8aa4182f449f0c480
williamrygao <wimrygao@gmail.com>
2024-10-24 16:55:24 -0400

    Add the Logout Use Case to Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -12,13 +12,13 @@ public class Main {
      */
     public static void main(String[] args) {
         final AppBuilder appBuilder = new AppBuilder();
-        // TODO: add the Logout Use Case to the app using the appBuilder
         final JFrame application = appBuilder
                                             .addLoginView()
                                             .addSignupView()
                                             .addLoggedInView()
                                             .addSignupUseCase()
                                             .addLoginUseCase()
+                                            .addLogoutUseCase()
                                             .addChangePasswordUseCase()
                                             .build();
 
