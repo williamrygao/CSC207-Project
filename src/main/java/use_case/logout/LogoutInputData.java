0a2729be4ee8ed94d5007b5b5ba0ad2689830402
Victor Li <victorli@Victors-MacBook-Air-2.local>
2024-10-25 09:13:51 -0400

    Completed parts 3 and 4.

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -5,8 +5,13 @@ package use_case.logout;
  */
 public class LogoutInputData {
 
+    private final String username;
+
     public LogoutInputData(String username) {
-        // TODO: save the current username in an instance variable and add a getter.
+        this.username = username;
     }
 
+    public String getUsername() {
+        return username;
+    }
 }
