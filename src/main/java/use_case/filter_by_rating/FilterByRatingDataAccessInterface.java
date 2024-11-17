82f54830c315c8ea532510d480988d5c0e134ec8
Victor Li <victorli@Victors-MacBook-Air-2.local>
2024-11-17 13:18:41 -0500

    Finished the FilterByRatingDataAccessInterface and FilterByRatingOutputData.

--- src/main/java/use_case/filter_by_rating/FilterByRatingDataAccessInterface.java
+++ src/main/java/use_case/filter_by_rating/FilterByRatingDataAccessInterface.java
@@ -1,4 +1,9 @@
 package use_case.filter_by_rating;
 
+import entity.Book;
+
+import java.util.List;
+
 public interface FilterByRatingDataAccessInterface {
+    List<Book> filterByRating(int rating);
 }
