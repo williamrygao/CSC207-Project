0b1a59f8793f9675b284568fba2966742c7c4ff4
ZefW <zfzefeng.wu@mail.utoronto.ca>
2024-11-26 23:22:46 -0500

    complete LeaveRatingInputData

--- src/main/java/use_case/leave_rating/LeaveRatingInputData.java
+++ src/main/java/use_case/leave_rating/LeaveRatingInputData.java
@@ -1,8 +1,6 @@
 package use_case.leave_rating;
 
 import entity.Listing;
-import entity.Rating;
-import entity.User;
 
 /**
  * The Input Data for the LeaveRating use case.
@@ -10,28 +8,54 @@ import entity.User;
 public class LeaveRatingInputData {
 
     private final String username;
+
     private final String password;
+
     private final Listing listing;
+
     private final String bookid;
-    private final Rating rating;
+
     private final Integer newRating;
 
-    /**
+     /**
      * LeaveRatingInputData method.
      *
      * @param username the username
      * @param password the password
-     * @param listing the listing
+     * @param listing  the listing
+     * @param newRating  the new rating
      */
-    public LeaveRatingInputData(final String username, final String password,, Listing listing, final Integer newRating) {
+     public LeaveRatingInputData(final String username, final String password, Listing listing, final Integer newRating) {
         this.username = username;
         this.password = password;
         this.bookid = password;
         this.listing = listing;
         this.newRating = newRating;
-
     }
 
-}
+    /**
+     * * Getter for username.
+     *
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
 
+    public Listing getListing() {
+        return listing;
+    }
+
+    public String getBookid() {
+        return bookid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Integer getNewRating() {
+        return newRating;
+    }
+}
 
