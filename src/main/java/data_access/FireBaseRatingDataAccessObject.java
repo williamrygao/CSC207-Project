b95f998fb814a6ba68727cf01a7a5fbc13dafb31
ZefW <zfzefeng.wu@mail.utoronto.ca>
2024-11-26 23:01:38 -0500

    create FireBaseRatingDataAccessObject.java

---
+++ src/main/java/data_access/FireBaseRatingDataAccessObject.java
@@ -0,0 +1,19 @@
+package data_access;
+
+
+import entity.BookFactory;
+import entity.Listing;
+import okhttp3.*;
+import org.json.JSONException;
+import org.json.JSONObject;
+import use_case.login.LoginListingDataAccessInterface;
+import use_case.sell.SellBookDataAccessInterface;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class FireBaseRatingDataAccessObject {
+
+}
