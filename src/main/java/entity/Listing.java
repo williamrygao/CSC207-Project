f06e8aa851071bb1da4ff4fdbd7cf93cc2ba09ff
graoly <roy.gal@mail.utoronto.ca>
2024-11-27 14:45:35 -0500

    Minor changes such as wording of instance variables.

--- src/main/java/entity/Listing.java
+++ src/main/java/entity/Listing.java
@@ -7,15 +7,15 @@ public class Listing {
 
     private String listingID;
     private Book book;
-    private String sellingPrice;
+    private String price;
     private String seller;
     private boolean isAvailable;
 
     // Constructor, getters, and setters
-    public Listing(String listingID, Book book, String sellingPrice, String seller, boolean isAvailable) {
+    public Listing(String listingID, Book book, String price, String seller, boolean isAvailable) {
         this.listingID = listingID;
         this.book = book;
-        this.sellingPrice = sellingPrice;
+        this.price = price;
         this.seller = seller;
         this.isAvailable = isAvailable;
     }
@@ -37,12 +37,12 @@ public class Listing {
         this.book = book;
     }
 
-    public String getSellingPrice() {
-        return sellingPrice;
+    public String getPrice() {
+        return price;
     }
 
     public void setPrice(String price) {
-        this.sellingPrice = price;
+        this.price = price;
     }
 
     public String getSeller() {
