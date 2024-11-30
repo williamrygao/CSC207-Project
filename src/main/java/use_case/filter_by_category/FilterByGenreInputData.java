d024bea75b1691fa3c636f8927ee72b3fbbb79bd
tanusharora <tanush.arora@mail.utoronto.ca>
2024-11-30 11:36:56 -0500

    Refactored filter_by_category to relocate files to use_case package

--- src/main/java/use_case/filter_by_category/FilterByGenreInputData.java
+++ src/main/java/use_case/filter_by_category/FilterByGenreInputData.java
@@ -1,4 +1,4 @@
-package interface_adapter.filter_by_category;
+package use_case.filter_by_category;
 
 /**
  * Deals with data structure representing the input data for the "Filter Books by Genre" use case.
