dc7b2598ef9460340de8e615ed62f6bf3e45d4d2
williamrygao <wimrygao@gmail.com>
2024-12-01 09:39:03 -0500

    git status

--- src/main/java/use_case/wishlist/add_to_wishlist/AddToWishlistUserDataAccessInterface.java
+++ src/main/java/use_case/wishlist/add_to_wishlist/AddToWishlistUserDataAccessInterface.java
@@ -4,10 +4,6 @@ import entity.Listing;
 import entity.user.User;
 
 public interface AddToWishlistUserDataAccessInterface {
-    String getCurrentUsername();
-
-    void setCurrentUsername(String username);
-
     void addToWishlist(User user, Listing listing);
 
     User get(String username);
