99a78ed709b1455e90c6f28cef0d63e00bb27cdf
Victor Li <victorli@Victors-MacBook-Air-3.local>
2024-12-02 16:12:06 -0500

    Added input error handling for filter by price view.

--- src/main/java/interface_adapter/filter_by_price/FilterByPriceController.java
+++ src/main/java/interface_adapter/filter_by_price/FilterByPriceController.java
@@ -3,6 +3,9 @@ package interface_adapter.filter_by_price;
 import use_case.filter_by_price.FilterByPriceInputBoundary;
 import use_case.filter_by_price.FilterByPriceInputData;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * The controller for the Filter By Price Use Case.
  */
@@ -21,6 +24,15 @@ public class FilterByPriceController {
     public void execute(int maxPrice) {
         final FilterByPriceInputData filterByPriceInputData = new FilterByPriceInputData(maxPrice);
         filterByPriceInteractor.execute(filterByPriceInputData);
+    }
+
+    /**
+     * Executes error messages for the filter by price use case.
+     * @param errorMessage the error message to display to user
+     * @param center where the panel will be centered
+     */
+    public void error(Component center, String errorMessage) {
+        JOptionPane.showMessageDialog(center, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
     }
 }
 
