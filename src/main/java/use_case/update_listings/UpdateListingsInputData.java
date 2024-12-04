45360acbc4c67745c9b866c90cbc92f42e79ebcc
tanusharora <tanush.arora@mail.utoronto.ca>
2024-12-03 20:48:53 -0500

    Enhanced documentation for UpdateListingsInputData constructor and method to meet checkstyle standards.

--- src/main/java/use_case/update_listings/UpdateListingsInputData.java
+++ src/main/java/use_case/update_listings/UpdateListingsInputData.java
@@ -1,15 +1,28 @@
 package use_case.update_listings;
 
 /**
- * The Input Data for the Login Use Case.
+ * A data structure that encapsulates the input data required for the Update Listings use case.
+ * This class adheres to the Data Transfer Object (DTO) design pattern,
+ * facilitating the transfer of information across the application's layers.
  */
 public class UpdateListingsInputData {
 
     private final String username;
+    /**
+     * Constructs an UpdateListingsInputData object.
+     *
+     * @param username the username of the user whose listings are to be updated.
+     */
 
     public UpdateListingsInputData(String username) {
         this.username = username;
     }
+
+    /**
+     * Retrieves the username associated with this input data.
+     *
+     * @return the username
+     */
 
     String getUsername() {
         return username;
